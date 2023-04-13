c903954bc73d91c0832a7446e37f02d58e1c80f7
Arno luyckx <A.luyckx@protonmail.com>
2023-04-14 00:23:35 +0200

    ..

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Altcademy_MF_gh-pages/",
+  base: "/ALTCADEMY_MF_GH-PAGES/",
 });
